9c9ec913d5aa9372c4af3fc1fd38a981063ba184
Anders Sjögren <asj@Oxymat.local>
2017-11-19 11:28:20 +0100

    Added java classes for model, customer and order

---
+++ EksamenProject/src/main/java/oxymat/demo/Model.java
@@ -0,0 +1,26 @@
+package oxymat.demo;
+
+public class Model {
+    private String name;
+    private int price;
+
+    public Model(){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String value){
+        this.name = value;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int value){
+        this.price = value;
+    }
+}
